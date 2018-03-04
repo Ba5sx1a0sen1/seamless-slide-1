73efb8ea3ff7a8fc013acf0b55b213e58dfb6b5c
BassXiaoSen <314121378@qq.com>
2018-03-04 16:10:11 +0800

    有bug全部作废

--- main.js
+++ main.js
@@ -1,13 +1,7 @@
 let index = 0
+let timer
 init() //初始化
-setInterval(()=>{
-    makeLeave(getImage(index))
-        .one('transitionend',(e)=>{
-            makeEnter($(e.currentTarget))
-        })
-    makeCurrent(getImage(index+1))
-    index++
-},2000)
+timer = setTimer()
 
 //工具函数
 function getIndex(n){return n%5+1}
@@ -16,3 +10,18 @@ function init(){getImage(index).addClass('current').siblings().addClass('enter')
 function makeCurrent($node){return $node.removeClass('enter leave').addClass('current')}
 function makeLeave($node){return $node.removeClass('enter current').addClass('leave')}
 function makeEnter($node){return $node.removeClass('current leave').addClass('enter')}
+function setTimer(){
+    return setInterval(()=>{
+        makeLeave(getImage(index))
+            .one('transitionend',(e)=>{
+                makeEnter($(e.currentTarget))
+            })
+        makeCurrent(getImage(index+1))
+        index++
+    },1000)
+}
+
+document.addEventListener('visibilitychange',function(e){
+    console.log(document.hidden)
+    document.hidden?window.clearInterval(timer):timer=setTimer()
+})
